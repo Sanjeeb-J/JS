4cf9be524e9d373640f2148b561a88575e9a7ee5
Sanjeeb J <sanjeebkodoth@gmail.com>
2025-01-26 18:23:08 +0530

    Jan 26

---
+++ script.js
@@ -0,0 +1,33 @@
+const students = ["Spongebob", "Patrick", "Squidward", "Sandy"];
+
+students.forEach(upperCase);
+students.forEach(display);
+
+function upperCase(element, index, array){
+    array[index] = element.toUpperCase();
+}
+function display(element){
+    console.log(element);
+}
+
+
+
+
+
+// By using forEach()
+const students = ["Spongebob", "Patrick", "Squidward", "Sandy"];
+
+students.forEach(upperCase);
+students.forEach(display);
+
+function upperCase(element, index, array){
+    array[index] = element.toUpperCase();
+}
+function display(element){
+    console.log(element);
+}
+
+
+
+
+
