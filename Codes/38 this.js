6497534de22cc009999b4b0548a5abc28e728d35
Sanjeeb J <sanjeebkodoth@gmail.com>
2025-01-30 09:18:02 +0530

    Jan 30

---
+++ Codes/38 this.js
@@ -0,0 +1,33 @@
+// this = reference to the object where THIS is used
+//        (the object depends on the immediate context)
+//        person.name = this.name
+
+/*
+    this function does not works with arrow function
+*/
+
+const person1 = {
+    name: "Spongebob",
+    favFood: "hamburgers",
+    sayHello: function(){console.log(`Hi! I am ${this.name}`)},
+    eat: function(){console.log(`${this.name} is eating ${this.favFood}`)}
+}
+
+const person2 = {
+    name: "Patrick",
+    favFood: "pizza",
+    sayHello: function(){console.log(`Hi! I am ${this.name}`)},
+    eat: function(){console.log(`${this.name} is eating ${this.favFood}`)}
+}
+
+person1.sayHello();
+person1.eat();
+
+person2.sayHello();
+person2.eat();
+
+
+
+
+
+console.log(this);
