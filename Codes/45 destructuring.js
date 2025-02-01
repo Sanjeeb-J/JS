70df665c6560bb53e9690799a62854f6de7b187e
Sanjeeb J <sanjeebkodoth@gmail.com>
2025-02-01 15:19:22 +0530

    Feb 1

--- Codes/45 destructuring.js
+++ Codes/45 destructuring.js
@@ -78,3 +78,27 @@ console.log(job);
 
 
 
+// ---------- EXAMPLE 5 -----------
+// DESTRUCTURE IN FUNCTION PARAMETERS
+
+function displayPerson({firstName, lastName, age, job="Unemployed"}){
+    console.log(`Name: ${firstName} ${lastName}`);
+    console.log(`Age: ${age}`);
+    console.log(`Job: ${job}`);
+}
+
+const person1 = {
+    firstName: "Spongebob",
+    lastName: "SquarePants",
+    age: 30,
+    job: "Fry Cook",
+}
+
+const person2 = {
+    firstName: "Patrick",
+    lastName: "Star",
+    age: 34
+}
+
+displayPerson(person1);
+displayPerson(person2);
